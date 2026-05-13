95f72e6e9916c95d94f0563651c6e283add7a93a
francis <franzdiaz460@protonmail.com>
2026-05-13 20:19:59 +0400

    test: create test for getapikey

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+
+	test := []struct {
+		input string
+		want  string
+	}{
+		{input: "ApiKey 1234566", want: "ApiKey 1234566"},
+		{input: "1234566", want: "ApiKey 1234566"},
+		{input: "", want: "ApiKey 1234566"},
+		{input: "ApiKey", want: "ApiKey 1234566"},
+	}
+
+	for _, ts := range test {
+		header := http.Header{}
+		header.Set("Authorization", ts.input)
+		_, err := GetAPIKey(header)
+		if err != nil {
+			fmt.Printf("[TEST] - Input: '%s', Test Error: %s\n", ts.input, err)
+			t.Fail()
+		}
+	}
+}
